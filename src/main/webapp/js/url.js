9e3613293218ce2189173c591eaf16f7741876f3
廖金龙 <liaojl@koal.com>
2018-11-20 20:55:11 +0800

    脚本更新

--- src/main/webapp/js/url.js
+++ src/main/webapp/js/url.js
@@ -7,7 +7,7 @@ function doUrl(logUrl,tocurl,id) {
 				goodid :id			},
 			complete : function(xhr, textStatus) {
 				// called when complete
-				window.open(tocurl);
+				location.href =tocurl;
 			}
 		});
 }
@@ -46,7 +46,7 @@ if ($(window).width() < 640) {
 }
 
 $(function() {
-	var delay = 3000;
+	var delay = 5000;
 	var list=$('#box li');
 	var index=0;
 	var length=list.length;
